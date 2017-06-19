23138b56dd923168954fd3d03eaab2e7651add19
Ilya Bashilov <tango1603@yandex.ru>
2017-06-19 19:49:20 +0300

    Add project files.

---
+++ MVCAngular/Scripts/MyApp.js
@@ -0,0 +1,10 @@
+﻿(function () {
+
+    //create a module
+    var app = angular.module('MyApp', ['ngRoute']);
+
+    //create controller
+    app.controller('HomeController', function ($scope) {
+        $scope.Message = "Yahoo!!!";
+    });
+})();
